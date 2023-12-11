70478e7230d4cdf0c37cd6a46bf14c3ff4c35440
selwy6208 <hamilton608@gmail.com>
2023-12-12 01:51:38 +0900

    up: query line

--- IntacctPostInvoices.py
+++ IntacctPostInvoices.py
@@ -1,76 +1,101 @@
 from http import client
-from ssl import create_default_context
-from turtle import update
-from xml.dom.minidom import Document 
-import urllib.request
-from xml.dom.minidom import parse
-#from matplotlib import projections
-import requests
-import time
-import xml.etree.ElementTree as ET
-import datetime
 import os
-from time import sleep 
-from datetime import date, timedelta
+import time
 import pyodbc 
-  
-server = 'lbmcbenefits.database.windows.net'
-database = 'LBMCbenefits'
-username = 'LBMC@lbmcbenefits'
-password = '3fP3Z4AE69tgyOBoa3sF'
+import requests
+import datetime
+from time import sleep 
+from turtle import update
+import urllib.request
+from xml.dom.minidom import Document 
+from xml.dom.minidom import parse
+from ssl import create_default_context
+#from matplotlib import projections
+import xml.etree.ElementTree as ET
+from datetime import date, timedelta
 
-connection_string = (
-    f'DRIVER={{ODBC Driver 17 for SQL Server}};'
-    f'SERVER={server};'
-    f'DATABASE={database};'
-    f'UID={username};'
-    f'PWD={password};'
-)
-
-# Establish a connection
-conn = pyodbc.connect(connection_string)
-print("Connection to SQL Server successful.")
-
-# Create a cursor for executing SQL queries
-cursor = conn.cursor()
-
-ENDPOINT_URL = "https://api.intacct.com/ia/xml/xmlgw.phtml"
+senderId = "LBMC"
+senderPassword = "2t2fPXW!!&lt;9y"
+amt = 0
+companyId = "LBMC"
+userId = "DWReader"
+userPassword = "$KgWYS168TB"
+    
 TIMEOUT = 90
+ENDPOINT_URL = "https://api.intacct.com/ia/xml/xmlgw.phtml"
+DATABASE_DRIVER = 'ODBC Driver 17 for SQL Server'
+INVOICE_SUB_STR = 'Benefits Invoice - September 2023.xlsx'
+YEAR_MONTH = 'September 2023'
+DB_CONFIG = {
+    'server': 'lbmcbenefits.database.windows.net',
+    'database': 'LBMCbenefits',
+    'username': 'LBMC@lbmcbenefits',
+    'password': '3fP3Z4AE69tgyOBoa3sF',
+}
 
 class XMLRequestClient:
-
-    def __init__(self):
-        pass
-
     @staticmethod
     def post(request):
-        # Set up the url Request class and use this Content Type
-        # to avoid urlencoding everything
         header = {'Content-type': 'application/xml'}
         conn = urllib.request.Request(ENDPOINT_URL, headers = header, method='POST')
 
-        # Post the request
         result = urllib.request.urlopen(conn, request.toxml(encoding="ascii"), TIMEOUT)
         
-        # Check the HTTP code is 200-OK
         if result.getcode() != 200:
-            # Log some of the info for debugging
             raise Exception("Received HTTP status code, " + result.getcode())
 
-        # Load the XML into the response
         response = parse(result)
         return response
+    
+def establish_connection():
+    connection_string = (
+        f'DRIVER={{{DATABASE_DRIVER}}};'
+        f'SERVER={DB_CONFIG["server"]};'
+        f'DATABASE={DB_CONFIG["database"]};'
+        f'UID={DB_CONFIG["username"]};'
+        f'PWD={DB_CONFIG["password"]};'
+    )
+
+    try:
+        with pyodbc.connect(connection_string) as conn:
+            print("Connection to SQL Server successful.")
+            return conn
+    except Exception as e:
+        print(f"Error establishing connection: {e}")
+        raise
 
 def get_session():
-    controlid = str(time.time()).replace('.', '')
-    companyid = "LBMC"
-    userid = "DWReader"
-    userpassword = "$KgWYS168TB"
-    
-    url = "https://api.intacct.com/ia/xml/xmlgw.phtml"
+    controlId = str(time.time()).replace('.', '')
+
     header = {'Content-type': 'application/xml'}
-    payload = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\n<request>\r\n  <control>\r\n    <senderid>"+senderId+"</senderid>\r\n    <password>"+senderPassword+"</password>\r\n    <controlid>"+controlid+"</controlid>\r\n    <uniqueid>false</uniqueid>\r\n    <dtdversion>3.0</dtdversion>\r\n    <includewhitespace>false</includewhitespace>\r\n  </control>\r\n  <operation>\r\n    <authentication>\r\n      <login>\r\n        <userid>"+userid+"</userid>\r\n        <companyid>"+companyid+"</companyid>\r\n        <password>"+userpassword+"</password>\r\n  <locationid>101</locationid>\r\n    </login>\r\n    </authentication>\r\n    <content>\r\n      <function controlid=\"1ee01cfe-aa00-4931-9731-f8591a0e54d2\">\r\n        <getAPISession />\r\n      </function>\r\n    </content>\r\n  </operation>\r\n</request>"     
-    response = requests.request("POST", url, data=payload, headers=header)
+    payload = f"""<?xml version="1.0" encoding="UTF-8"?>
+        <request>
+          <control>
+            <senderid>{senderId}</senderid>
+            <password>{senderPassword}</password>
+            <controlid>{controlId}</controlid>
+            <uniqueid>false</uniqueid>
+            <dtdversion>3.0</dtdversion>
+            <includewhitespace>false</includewhitespace>
+          </control>
+          <operation>
+            <authentication>
+              <login>
+                <userid>{userId}</userid>
+                <companyid>{companyId}</companyid>
+                <password>{userPassword}</password>
+                <locationid>101</locationid>
+              </login>
+            </authentication>
+            <content>
+              <function controlid="1ee01cfe-aa00-4931-9731-f8591a0e54d2">
+                <getAPISession />
+              </function>
+            </content>
+          </operation>
+        </request>"""
+    
+    response = requests.request("POST", ENDPOINT_URL, data=payload, headers=header)
     #print(response.text)
     root = ET.fromstring(response.content)
 
@@ -78,23 +103,27 @@ def get_session():
         session = (child.text)
     return session
 
-def get_clients():
+def get_clients(cursor):
     sql = """
         set nocount on 
-        select distinct clientcode from dbo.[BILLING_STEP_3]
+        select distinct ClientCode from dbo.[BILLING_STEP_3]
         order by 1 desc
     """
     cursor.execute(sql)
     row = cursor.fetchall() 
     return  row 
 
-def get_detail(c):
+def get_detail(clientCode, cursor):
     sql = """
-    select IIF(premium IS NULL, 0, premium) as premium
-	, lastname + ', ' + firstname
+    SET NOCOUNT ON
+    SELECT 
+    IIF(premium IS NULL, 0, premium) as premium
+	, lastname + ', ' + firstname as fullName
 	, period
 	, Customer_ID
 	, project_ID
+    , ClientName
+	, ClientCode 
     ,case when [plan] like '%bcbs%dental%' then 'EP-BCBS-DENTAL'
         when [plan] like '%bcbs%(%)%' then 'EP-BCBS-HEALTH'
         when [plan] like '%bcbs%vision%' then 'EP-BCBS-VISION'
@@ -116,38 +145,35 @@ def get_detail(c):
         when [Provider Name] like '%lincoln%' and [plan] like '%short%term disability%' then 'EP-LINCOLN-STD'
         when [Provider Name] like '%lincoln%' and [plan] like '%supplemental life ins%' then 'EP-LINCOLN-LIFE'
         END  itemid
-    , clientname
-	, clientcode 
     from dbo.[BILLING_STEP_3]
     where clientcode = ? and
-     case when [plan] like '%bcbs%dental%' then 'EP-BCBS-DENTAL'
-        when [plan] like '%bcbs%(%)%' then 'EP-BCBS-HEALTH'
-        when [plan] like '%bcbs%vision%' then 'EP-BCBS-VISION'
-        when [plan] like '%cigna%p%/%' then 'EP-CIGNA-HEALTH'
-        when [plan] like '%cigna%dental%' then 'EP-CIGNA-DENTAL'
-        when [plan] like '%cigna%vision%' then 'EP-CIGNA-VISION'
-        when [plan] like '%cigna%dental%' then 'EP-CIGNA-DENTAL'
-        when [Provider Name] like '%symet%' then  'EP-SYMETRA-INDEMNITY'
-		when [plan] like '%symet%' then  'EP-SYMETRA-INDEMNITY'
-        when [Provider Name] like '%colonial%' and [plan] like '%critical%' then 'EP-COLONIAL-CRITICAL'
-		when [plan] like '%Colonial Critical Illness%' then 'EP-COLONIAL-CRITICAL'
-		when [plan] like '%Colonial Life Group Critical Care%'  then 'EP-COLONIAL-CRITICAL'
-        when [Provider Name] like '%colonial%' and [plan] like '%accid%' then 'EP-COLONIAL-ACCIDENT'
-		when [plan] like '%Colonial Accident Plan%' or [plan] like '%Colonial Life Group Accident%' then 'EP-COLONIAL-ACCIDENT'
-		when [plan] like '%Colonial Accident%' then 'EP-COLONIAL-ACCIDENT'
-        when [plan] like '%STANDARD LIFE%' AND [Provider Name] LIKE '%LINCOLN%' then 'EP-LINCOLN-STD'
-        when [Provider Name] like '%lincoln%' and [plan] like '%long term disability%' then 'EP-LINCOLN-LTD'
-        when [Provider Name] like '%lincoln%' and [plan] like '%vol%short%term disability%' then 'EP-LINCOLN-STD-VOL'
-        when [Provider Name] like '%lincoln%' and [plan] like '%short%term disability%' then 'EP-LINCOLN-STD'
-        when [Provider Name] like '%lincoln%' and [plan] like '%supplemental life ins%' then 'EP-LINCOLN-LIFE'
-        END IS NOT NULL
+        case when [plan] like '%bcbs%dental%' then 'EP-BCBS-DENTAL'
+            when [plan] like '%bcbs%(%)%' then 'EP-BCBS-HEALTH'
+            when [plan] like '%bcbs%vision%' then 'EP-BCBS-VISION'
+            when [plan] like '%cigna%p%/%' then 'EP-CIGNA-HEALTH'
+            when [plan] like '%cigna%dental%' then 'EP-CIGNA-DENTAL'
+            when [plan] like '%cigna%vision%' then 'EP-CIGNA-VISION'
+            when [plan] like '%cigna%dental%' then 'EP-CIGNA-DENTAL'
+            when [Provider Name] like '%symet%' then  'EP-SYMETRA-INDEMNITY'
+            when [plan] like '%symet%' then  'EP-SYMETRA-INDEMNITY'
+            when [Provider Name] like '%colonial%' and [plan] like '%critical%' then 'EP-COLONIAL-CRITICAL'
+            when [plan] like '%Colonial Critical Illness%' then 'EP-COLONIAL-CRITICAL'
+            when [plan] like '%Colonial Life Group Critical Care%'  then 'EP-COLONIAL-CRITICAL'
+            when [Provider Name] like '%colonial%' and [plan] like '%accid%' then 'EP-COLONIAL-ACCIDENT'
+            when [plan] like '%Colonial Accident Plan%' or [plan] like '%Colonial Life Group Accident%' then 'EP-COLONIAL-ACCIDENT'
+            when [plan] like '%Colonial Accident%' then 'EP-COLONIAL-ACCIDENT'
+            when [plan] like '%STANDARD LIFE%' AND [Provider Name] LIKE '%LINCOLN%' then 'EP-LINCOLN-STD'
+            when [Provider Name] like '%lincoln%' and [plan] like '%long term disability%' then 'EP-LINCOLN-LTD'
+            when [Provider Name] like '%lincoln%' and [plan] like '%vol%short%term disability%' then 'EP-LINCOLN-STD-VOL'
+            when [Provider Name] like '%lincoln%' and [plan] like '%short%term disability%' then 'EP-LINCOLN-STD'
+            when [Provider Name] like '%lincoln%' and [plan] like '%supplemental life ins%' then 'EP-LINCOLN-LIFE'
+            END IS NOT NULL
     """
-    
-    cursor.execute(sql,c)
+    cursor.execute(sql, clientCode)
     row = cursor.fetchall() 
     return  row 
  
-def post_data(projectID, customerID, amt, createDate, cdYear, cdMonth, cdDay, invoiceItems,customer,clientID,contacts):
+def post_data(conn, cursor, sessionId, projectID, customerID, amt, createDate, cdYear, cdMonth, cdDay, invoiceItems,customer,clientID,contacts):
     print(projectID, customerID, amt, createDate, cdYear, cdMonth, cdDay, invoiceItems,customer,clientID,contacts, "all data test")
     newdoc = Document();
     request = newdoc.createElement('request')
@@ -287,34 +313,34 @@ def post_data(projectID, customerID, amt, createDate, cdYear, cdMonth, cdDay, in
     
     if """<status>failure</status>""" in xmlData and contacts == 0:
       print("Failure, trying again without project contacts")
-      post_data(projectID, customerID, amt, createDate, cdYear, cdMonth, cdDay, invoiceItems, customer,clientID,1)
+    #   post_data(conn, cursor, sessionId, projectID, customerID, amt, createDate, cdYear, cdMonth, cdDay, invoiceItems, customer, clientID, 1)
       
     if """<status>failure</status>""" in xmlData and contacts == 1:
       print("Failure, moving on. Something else is failing.") 
     
- 
 #Begin
-senderId = "LBMC"
-senderPassword = "2t2fPXW!!&lt;9y"
+def main():
+    todaysDate = date.today()
+    createDate = todaysDate
+    cdMonth = todaysDate.month
+    cdYear = todaysDate.year
+    cdDay = todaysDate.day  
 
-sessionId = get_session()  
- 
-amt = 0
-todays_date = date.today()
-createDate = '08/22/2023'
-cdMonth = 8
-cdYear = 2023
-cdDay = 22
+    conn = establish_connection()
+    cursor = conn.cursor()
 
+    sessionId = get_session() 
+    clients = get_clients(cursor)
 
+    for clientCode in clients:
+        invoiceItems = get_detail(clientCode, cursor)
+        projectID = invoiceItems[0][4]
+        customerID = invoiceItems[0][3]
+        customer = invoiceItems[0][5]
+        clientID = invoiceItems[0][6]
 
-clients = get_clients()
-for c in clients:
-    invoiceItems = get_detail(c)
-    projectID = invoiceItems[0][4]
-    customerID = invoiceItems[0][3]
-    customer = invoiceItems[0][6]
-    clientID = invoiceItems[0][7]
+        post_data(conn, cursor, sessionId, projectID, customerID, amt, createDate, cdYear, cdMonth, cdDay, invoiceItems, customer,clientID, 0)
 
-    post_data(projectID, customerID, amt, createDate, cdYear, cdMonth, cdDay, invoiceItems, customer,clientID,0)
+if __name__ == "__main__":
+    main()
  
